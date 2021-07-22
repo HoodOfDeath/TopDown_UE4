e7e4145d14825d5025ab214ddf5f1415b6ca935d
HoodOfDeathST <k.namavir@gmail.com>
2021-07-22 09:47:28 +0300

    Added project name to use in includes.

--- ue4/TopDown_UE4/Source/TopDown_UE4/TopDown_UE4.Build.cs
+++ ue4/TopDown_UE4/Source/TopDown_UE4/TopDown_UE4.Build.cs
@@ -11,6 +11,8 @@ public class TopDown_UE4 : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		PrivateIncludePaths.AddRange(new []{ Name});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
